e7cb48ab72a47d4baf1e54bf42d8844e755486f6
Thomas Leger <thomasleger1@gmail.com>
2014-05-10 11:59:17 +0100

    minor correction

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 data <- read.table("household_power_consumption.txt", sep=";", as.is=TRUE, header=TRUE)
-
+sub<-subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
 hist(as.numeric(sub$Global_active_power), main = "Global Active Power", xlab="Global Active Power (kilowatts)",ylab="Frequency",col="Red")
 
 dev.copy(png, file = "plot1.png", width = 480, height = 480, units = "px")
